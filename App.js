a23518bcd50954c17059d944187097aa13d3f2ce
Filip Madunicky <yelidric@gmail.com>
2026-04-12 12:55:54 +0200

    Configures project for unit testing with Jest
    Integrates Jest and Jest-Expo for robust unit testing capabilities.
    Adds a `test` script and necessary Jest configurations to `package.json`.
    Extends ESLint configuration with `eslint-plugin-jest` to ensure proper linting of test files.
    Includes a basic example test to validate the setup.
    Removes an unused `Constants` import and `console.log` statement from `App.js`.

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client/react";
-import Constants from "expo-constants";
 
 import "./global.css";
 import Main from "./src/components/Main";
@@ -14,8 +13,6 @@ const authStorage = new AuthStorage();
 const apolloClient = createApolloClient(authStorage);
 
 export default function App() {
-  console.log(Constants.expoConfig);
-
   return (
     <SafeAreaProvider>
       <NativeRouter
